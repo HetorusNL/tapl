8ca1fd6a196586fda41e891f95dd455e73093e40
HetorusNL <tim@hetorus.nl>
2025-01-02 23:16:10 +0100

    initial implementation of c-code generation, compilation and execution of executable

--- src/compilers/compyler/__main__.py
+++ src/compilers/compyler/__main__.py
@@ -1,25 +1,90 @@
 import argparse
+from os import system
+from pathlib import Path
 
 from .ast import AST
 from .ast_generator import AstGenerator
+from .code_generator import CodeGenerator
 from .tokenizer import Tokenizer
 from .tokens import Token
 
 
-def main():
-    # get the 'file' argument from the argument parser
+def argument_parser() -> Path:
     parser = argparse.ArgumentParser()
     parser.add_argument("file")
     parsed_args = parser.parse_args()
+    return parsed_args.file
 
-    # tokenize the provided file
-    print(f"calling the compiler with file '{parsed_args.file}'")
-    tokens: list[Token] = Tokenizer(parsed_args.file).tokenize()
+
+def tokenize(file: Path) -> list[Token]:
+    print(f"calling the compiler with file '{file}'")
+    tokens: list[Token] = Tokenizer(file).tokenize()
     print(tokens)
+    return tokens
 
-    # generate an AST from the tokens
+
+def generate_ast(tokens: list[Token]) -> AST:
     ast: AST = AstGenerator(tokens).generate()
     print(*ast.expressions, sep="\n")
+    return ast
+
+
+def generate_code(ast: AST) -> list[str]:
+    c_code: list[str] = CodeGenerator(ast).generate_c()
+    print("``` c")
+    print(*c_code, sep="", end="")
+    print("```")
+    return c_code
+
+
+def write_file(c_code: list[str]) -> Path:
+    # get to the repo root folder, several levels up
+    repo_root: Path = Path(__file__).parents[3].resolve()
+    build_folder: Path = repo_root / "build" / "compyler"
+    # ensure the build folder exists
+    build_folder.mkdir(parents=True, exist_ok=True)
+    # create the full filename of the c source file
+    c_file: Path = build_folder / "main.c"
+    # write all lines to the file
+    with open(c_file, "w") as f:
+        f.writelines(c_code)
+    return c_file
+
+
+def compile_c(c_file: Path) -> Path:
+    executable: Path = c_file.parent / "main"
+    command: str = f"gcc -o {executable} {c_file}"
+    print(command)
+    system(command)
+    return executable
+
+
+def run_executable(executable: Path):
+    print(executable)
+    system(executable)
+
+
+def main():
+    # get the 'file' argument from the argument parser
+    file: Path = argument_parser()
+
+    # tokenize the provided file
+    tokens: list[Token] = tokenize(file)
+
+    # generate an AST from the tokens
+    ast: AST = generate_ast(tokens)
+
+    # generate c-code from the AST
+    c_code: list[str] = generate_code(ast)
+
+    # write the code to main.c in the build folder
+    c_file: Path = write_file(c_code)
+
+    # run the c compiler to compile the file
+    executable: Path = compile_c(c_file)
+
+    # run the executable
+    run_executable(executable)
 
 
 if __name__ == "__main__":
---
+++ src/compilers/compyler/code_generator.py
@@ -0,0 +1,25 @@
+from .ast import AST
+
+
+class CodeGenerator:
+    def __init__(self, ast: AST):
+        self._ast = ast
+
+    def generate_c(self) -> list[str]:
+        # add the initial lines of code
+        c_code: list[str] = [
+            "#include <stdio.h>\n",
+            "\n",
+            "int main(int argc, char** argv) {\n",
+            '    printf("hello world!\\n");\n',
+        ]
+
+        # compile the expressions in the AST to code
+        for index, expression in enumerate(self._ast.expressions):
+            c_code.append(f'    printf("expression {index+1}: %d\\n", {expression});\n')
+
+        # add the ending lines of code
+        c_code.append("}\n")
+
+        # return the full source code
+        return c_code
