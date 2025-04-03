042d13029dc9f03654ad67cbe5833a3b1b3ebbe0
HetorusNL <tim@hetorus.nl>
2025-04-04 00:00:32 +0200

    added print statements and variables to the ast generator tests

--- src/compilers/test_compyler/ast_generator/test_ast_generator.py
+++ src/compilers/test_compyler/ast_generator/test_ast_generator.py
@@ -11,6 +11,9 @@ from compyler.ast_generator import AstGenerator
 from compyler.statements.statement import Statement
 from compyler.tokenizer import Tokenizer
 from compyler.tokens.token import Token
+from compyler.types.type_applier import TypeApplier
+from compyler.types.type_resolver import TypeResolver
+from compyler.types.types import Types
 from compyler.utils.ast import AST
 from compyler.utils.stream import Stream
 
@@ -23,15 +26,25 @@ class TestAstGenerator(unittest.TestCase):
         # make sure to pass a resolved path to the tokenizer and ast generator
         this_folder: Path = Path(__file__).parent.resolve()
         example_file: Path = this_folder / "example_statements.tim"
+        # tokenize the file to a stream
         tokens: Stream[Token] = Tokenizer(example_file).tokenize()
+        # resolve the types in the file
+        type_resolver: TypeResolver = TypeResolver(tokens)
+        types: Types = type_resolver.resolve()
+        # apply the types to the tokens in the stream
+        type_applier: TypeApplier = TypeApplier(types)
+        type_applier.apply(tokens)
+        # generate the ast and resulting statements to verify
         ast: AST = AstGenerator(tokens).generate()
         ast_statements: list[Statement] = ast.statements.objects
         print(*ast_statements, sep="\n")
 
-        # or simpler converting the individual statements to str
+        # convert the individual statements to str to compare
         result = [
             "(((1100 + (150 * 2)) + 37) - 100);",
             "((1 * 2) + (3 / ((4 + true))));",
             'printf("%d\\n", (1337));',
+            "u16 var = 10;",
+            'printf("%d\\n", (var));',
         ]
         self.assertEqual([statement.c_code() for statement in ast_statements], result)
